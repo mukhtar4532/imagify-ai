28bd23663e85f44fc09b412b09da2ae9bb8ef193
Mukhtar Hussain <mukhtarhussain4532@gmail.com>
2025-09-28 11:06:45 +0530

    Intialize the project with basic setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <div>
+      
+    <h1 className="bg-black text-white text-4xl">Hello, World!</h1>
+    <p>Welcome to your React application.</p>
+    </div>
+  )
+}
+
+export default App
